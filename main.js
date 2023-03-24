60626c96568d26c5f2ce18fe04eeac83bc8c7364
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2023-03-25 00:18:16 +0700

    JS Setup

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const firstName = document.getElementById('firstName'),
+    lastName = document.getElementById('lastName'),
+    userEmail = document.getElementById('userEmail'),
+    password = document.getElementById('password'),
+    trialForm = document.getElementById('trialForm');
+
+
+// Mail Check
+const mailCheck = (input) => {
+    const regEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    const testMail = input.value.trim();
+}
+
+// Error
+const formError = (input) => {
+
+}
+
+trialForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
