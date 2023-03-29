d836dc93e381e02b2ce7387af741a6703214f218
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2023-03-29 18:43:28 +0700

    Complete some functions

--- main.js
+++ main.js
@@ -6,11 +6,32 @@ const   trialForm = document.getElementById('trialForm'),
     userEmail = document.getElementById('userEmail'),
     password = document.getElementById('password');
 
+// Name check 
+const nameChecked = (input, input2) => {
+    const testFirstName = input.value.trim();
+    const testLastName = input2.value.trim();
+    const testName = [testFirstName, testLastName];
+
+    testName.toLowerCase();
+    testName.charAt(0).toUpperCase();
+
+    console.log(testName);
+}
 
 // Mail Check
-const mailCheck = (input) => {
+const mailChecked = (input) => {
     const regEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     const testMail = input.value.trim();
+
+    if (regEx.test(testMail)) {
+        const passMail = testMail.toLowerCase();
+        console.log(passMail);
+        return true;
+
+    }   else {
+        formError();
+        console.log(passMail);
+    }
 }
 
 // Error
@@ -20,4 +41,16 @@ const formError = (input) => {
 
 trialForm.addEventListener('submit', (e) => {
     e.preventDefault();
-})
+
+    const yourName = nameChecked(firstName, lastName);
+    const eMail = mailChecked(userEmail);
+
+    switch (yourName && eMail) {
+        case true:
+            console.log('Pass');
+            break;
+        default:
+            console.log('Not Pass');
+            break;
+    }
+});
