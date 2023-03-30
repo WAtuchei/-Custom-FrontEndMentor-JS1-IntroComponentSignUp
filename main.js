d6e0963fe3446099caa34bb49b85f3d608065e8c
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2023-03-31 00:31:01 +0700

    Add name & length Check function

--- main.js
+++ main.js
@@ -8,29 +8,48 @@ const   trialForm = document.getElementById('trialForm'),
 
 // Name check 
 const nameChecked = (input, input2) => {
-    const testFirstName = input.value.trim();
-    const testLastName = input2.value.trim();
-    const testName = [testFirstName, testLastName];
+    const regEx = /^([A-Za-z]+[\s]?)+$/;
+    const testFirstName = input.value.trim().toLowerCase();
+    const testLastName = input2.value.trim().toLowerCase();
 
-    testName.toLowerCase();
-    testName.charAt(0).toUpperCase();
+    if (regEx.test(testLastName, testFirstName)) {
+        const allName = [testFirstName, testLastName];
+        const capitalizedName = allName.map((name) => name.charAt(0).toUpperCase() + name.slice(1));
 
-    console.log(testName);
+        console.log(capitalizedName);
+        console.log('name pass');
+        return true;
+
+    } else {
+        console.log('name fail');
+    }
 }
 
 // Mail Check
 const mailChecked = (input) => {
     const regEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    const testMail = input.value.trim();
+    const testMail = input.value.trim().toLowerCase();
 
     if (regEx.test(testMail)) {
-        const passMail = testMail.toLowerCase();
-        console.log(passMail);
+        console.log(`mail Pass ${testMail}`);
         return true;
 
-    }   else {
-        formError();
-        console.log(passMail);
+    } else {
+        // formError();
+        console.log(`mail Fail`);
+    }
+}
+
+// Length Check
+const lengthChecked = (input, min) => {
+    const testLength = input.value.trim();
+
+    if (testLength === "") {
+        console.log(`this is ${input.name}`);
+    } else if (testLength < min) {
+        console.log('lower min');
+    } else {
+        return true;
     }
 }
 
@@ -39,6 +58,7 @@ const formError = (input) => {
 
 }
 
+// Submit Form
 trialForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -47,10 +67,10 @@ trialForm.addEventListener('submit', (e) => {
 
     switch (yourName && eMail) {
         case true:
-            console.log('Pass');
+            console.log('Form Pass');
             break;
         default:
-            console.log('Not Pass');
+            console.log('Form Fail');
             break;
     }
 });
