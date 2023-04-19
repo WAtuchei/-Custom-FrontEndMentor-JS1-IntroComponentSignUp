f8ce411eda4fe88abe1e3f52e09956e895498a29
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2023-04-19 17:27:40 +0700

    Feat: error function

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ const nameChecked = (input, input2) => {
         console.log('double pass');
         // console.log(nameF.charAt(0).toUpperCase() + nameF.slice(1));
         return true;
-
     }
 }
 
@@ -33,17 +32,17 @@ const mailChecked = (input) => {
         return true;
 
     } else {
-        // formError();
+        formError(input, `Looks like this is not ${input.name}`);
     }
 }
 
 // Length Check
 const lengthChecked = (input, min) => {
     if (input.value === "") {
-        console.log(`${input.name} cannot be empty`);
+        formError(input, `${input.name} cannot be empty`);
 
     } else if (input.value.length < min) {
-        console.log(`Looks like this is not ${input.name}`);
+        formError(input, `Looks like this is not ${input.name}`);
         
     } else {
         return true;
@@ -51,8 +50,28 @@ const lengthChecked = (input, min) => {
 }
 
 // Error
-const formError = (input) => {
+const formError = (input, message) => {
+    const errorIcon = $(input).next(),
+        errorMessage = errorIcon.next();
 
+    input.style.borderColor = "red";
+    input.style.color = "red";
+    $(input).parent().addClass('error-shaking');
+    errorIcon.show();
+    errorIcon.addClass('fading-animated');
+    errorMessage.text(message);
+
+    input.parentElement.addEventListener('animationend', () => {
+        $(input).parent().removeClass('error-shaking');
+        errorIcon.removeClass('fading-animated');
+    });
+
+    input.addEventListener('keydown', () => {
+        errorIcon.hide();
+        errorMessage.text("");
+        input.style.borderColor = "rgba(128, 128, 128, 0.219)";
+        input.style.color = "#000";
+    });
 }
 
 // Submit Form
